84ef1bdb303ade91a6c91720ebf1dd8de5b44c72
刘沛霖 <136075808+JLU-Liupeilin@users.noreply.github.com>
2026-09-10 15:52:34 +0800

    Update __init__.py
    Reordered import statements and added __all__ for exports.

--- ridrec/data/__init__.py
+++ ridrec/data/__init__.py
@@ -1,2 +1,3 @@
-from ridrec.data.dataset import SessionBatch, SessionDataset, collate_sessions
-from ridrec.data.preprocess import SessionPreprocessor, PreprocessStats
+from ridrec.data.preprocess import PreprocessStats, SessionPreprocessor
+
+__all__ = ["PreprocessStats", "SessionPreprocessor"]
